0bbeed1b5fbe9c82499525083bb8769f360a40b9
Luis Enrique <Luis Enrique@LuisEnrique>
2016-06-27 22:14:25 -0600

    inicio

---
+++ src/chatcliente/Cliente.java
@@ -0,0 +1,81 @@
+package chatcliente;
+
+import chatservidor.HiloServidor;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JEditorPane;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.text.DefaultCaret;
+
+/**
+ *
+ * @author netosolis
+ */
+public class Cliente implements Runnable{
+    //Declaramos las variables necesarias para la conexion y comunicacion
+    private ArrayList lista;
+    private Socket cliente;
+    private DataInputStream in;
+    private DataOutputStream out;
+    //El puerto debe ser el mismo en el que escucha el servidor
+    private int puerto = 2027;
+    //Si estamos en nuestra misma maquina usamos localhost si no la ip de la maquina servidor
+    private String host = "192.168.0.11";
+    private String mensajes = "";
+    JEditorPane panel;
+    
+    //Constructor recibe como parametro el panel donde se mostraran los mensajes
+    public Cliente(JEditorPane panel){
+        this.panel = panel;
+        try {
+            cliente = new Socket(host,puerto);
+            in = new DataInputStream(cliente.getInputStream());
+            out = new DataOutputStream(cliente.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    @Override
+    public void run() {
+        try{
+            //Ciclo infinito que escucha por mensajes del servidor y los muestra en el panel
+            while(true){
+                lista=new ArrayList<String>();
+                mensajes += in.readUTF();
+                if(cliente.isConnected())
+                {
+                    for(int i=0;i<lista.size();i++)
+                    {
+                        panel.setText("\n"+(String) lista.get(i)+"\n");
+                    }
+                    panel.setText(mensajes);
+                    lista=null;
+                }
+                else
+                {
+                    lista.add(mensajes);
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    //Funcion sirve para enviar mensajes al servidor
+    public void enviarMsg(String msg){
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
